76e96495894f9d8c1565cd3358e39681af24502c
MarkBoomer <mark.boomer@9habu.com>
2020-04-16 23:17:17 +0100

    Add all project files
    Added all project files to github

---
+++ src/components/EventCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1 v-if="" v-show=""></h1>
+</template>
+
+<script>
+props: {
+  propName: {
+    type: Number,
+    default:
+  },
+},
+
+export default {
+data() {
+  return {
+    key: value
+  }
+},
+</script>
+
+<style></style>
